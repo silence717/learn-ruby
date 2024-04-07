fc331a641e91b4daefb1ea57617a1ad252b5c032
silence717 <yangfang2028@126.com>
2024-04-07 18:25:53 +0800

    chapter3 Strings and Ranges

---
+++ src/03/string_concat.rb
@@ -0,0 +1,11 @@
+s4 = "This ", "is", " not a string!", 10
+print("print (s4):", s4, "\n")
+
+
+s = "hello world"
+s = s + "!"
+print s
+
+s = "Hello world"
+
+print(s[1,3])
---
+++ src/03/string_index.rb
@@ -0,0 +1,4 @@
+s = "Hello world"
+
+puts(s[-5..5])  # this prints an empty string!
+puts(s[-5..-1])  # prints "world"
