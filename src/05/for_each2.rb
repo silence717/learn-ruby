3716f5b7cf185119e11c84fb7b711a451fb775cb
silence717 <yangfang2028@126.com>
2024-04-16 22:30:37 +0800

    chapter5 for loops

---
+++ src/05/for_each2.rb
@@ -0,0 +1,9 @@
+# for
+for s in 1..3
+  puts( s )
+end
+
+# each
+(1..3).each do |s|
+  puts(s)
+end
---
+++ src/05/for_loop.rb
@@ -0,0 +1,8 @@
+# This is Ruby code…
+for i in [1,2,3] do
+  puts( i )
+end
+
+for s in ['one','two','three'] do
+  puts( s )
+end
